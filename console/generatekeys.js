7cab6e6ba8eb114dca31fdd1bb61f5abaf16bb23
alberto@sgart.it <alberto@sgart.it>
2021-09-19 23:50:30 +0200

    modificato generatekeys

--- console/generatekeys.js
+++ console/generatekeys.js
@@ -1,9 +1,9 @@
 const webpush = require('web-push');
 
-const vapidKeys = webpush.generateVAPIDKeys()
+const {publicKey, privateKey} = webpush.generateVAPIDKeys()
 
 console.log("------------------------------------------------------------------------------------------------------");
-console.log(`publicKey: ${vapidKeys.publicKey}`);
+console.log(`publicKey: ${publicKey}`);
 console.log("------------------------------------------------------------------------------------------------------");
-console.log(`privateKey: ${vapidKeys.privateKey}`);
+console.log(`privateKey: ${privateKey}`);
 console.log("------------------------------------------------------------------------------------------------------");
